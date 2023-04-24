b9b3181ec7bcc32b516b31e0c1ae1bb38963e458
Carlos Diaz <carlos12one@hotmail.com>
2023-04-24 15:14:00 -0500

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Menu, Toolbar } from "@mui/material";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar sx={{ position: "static", background: "#4444", boxShadow: "none" }}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Menu />
+      </Toolbar>
+    </AppBar>
   );
-}
-
+};
 export default App;
