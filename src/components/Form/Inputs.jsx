3ac9e4ae2a3d637fa27542201b4868ba11eeaa3b
Carlos Diaz <carlos12one@hotmail.com>
2023-04-27 01:13:34 -0500

    add error line in component input

--- src/components/Form/Inputs.jsx
+++ src/components/Form/Inputs.jsx
@@ -1,13 +1,15 @@
 import { useField } from "formik";
 import { InlineError } from "../Notifications/Error";
-import { TextField } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 
 export const Inputs = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
     <>
-      <TextField margin="normal" fullWidth autoFocus label={label} {...field} {...props} />
-      {meta.touched && meta.error && <InlineError text={meta.error} />}
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <TextField margin="normal" fullWidth autoFocus label={label} {...field} {...props} />
+        {meta.touched && meta.error && <InlineError text={meta.error} />}
+      </Box>
     </>
   );
 };
